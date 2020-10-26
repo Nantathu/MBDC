45f962bf7b082cad85c1ea0e7268854ee185fae5
Nantathu <58397100+Nantathu@users.noreply.github.com>
2020-10-26 13:59:12 +0100

    Update 1.0.2
    Update. I have no idea how to describe it.

--- MBCounter.py
+++ MBCounter.py
@@ -4,8 +4,6 @@ from datetime import datetime
 import pathlib
 import pickle
 
-
-
 def button_click(number):
     current = entry.get()
     entry.delete(0, END)
@@ -22,7 +20,8 @@ def save_data():
 
 def load_data():
     if file.exists ():
-        var
+        global var
+        var = pickle.load(open('var_data.pickle', 'rb'))
 
 def button_clear():
     entry.delete(0, END)
@@ -31,19 +30,14 @@ def button_clear():
     variable = 0
 
 def button_log():
-    messagebox.showinfo('Last mental breakdown:', current_date)
-
-
+    messagebox.showinfo('Qbi moment last occured at:', current_date)
 
 #Main window functions and configuration.
 window = Tk()
-window.title("Mental Breakdown Counter")
-window.configure(bg="black")
+window.title("Qbi moment counter")
+window.configure(bg = "black")
 window.geometry("254x129")
 window.resizable(False, False)
-
-#Declaration of "var" variable to read data from pickle file.
-var = pickle.load(open('var_data.pickle', 'rb'))
 
 #Path of the pickle file that contains variable data.
 file = pathlib.Path('var_data.pickle')
@@ -55,6 +49,13 @@ current_date = datetime.today().strftime('%Y-%m-%d-%H:%M')
 entry = Entry(window, width="8")
 entry.grid(row = 0, column = 3, sticky = W)
 
+#Declaration of "var" variable to read data from pickle file.
+if file.exists() == True:
+    var = pickle.load(open('var_data.pickle', 'rb'))
+elif file.exists() == False:
+    variable = 0
+    pickle.dump(variable, open('var_data.pickle', 'wb'))
+
 #Checks if file exists to update var value
 if file.exists():
     entry.insert(0, var)
@@ -62,17 +63,17 @@ else:
     entry.insert(0, 0)
 
 #Buttons
-psycha = Label(window, text="Mental breakdown", bg="black", fg="white")
+psycha = Label(window, text = "Qbi moment: ", bg = "black", fg = "white")
 psycha.grid(row = 0, column = 1, sticky = E)
-button_psy = Button (window, text="MENTAL", bg="black", fg="white", activebackground="white", activeforeground="black", width="10", command=lambda: button_click(1))
+button_psy = Button (window, text = "QBI", bg = "black", fg = "white", activebackground = "white", activeforeground = "black", width = "10", command = lambda: button_click(1))
 button_psy.grid(row = 0, column = 0)
-button_clear = Button (window, text="CLEAR", bg="black", fg="white", activebackground="white", activeforeground="black", width="35", command = button_clear)
+button_clear = Button (window, text = "CLEAR", bg = "black", fg = "white", activebackground = "white", activeforeground = "black", width = "35", command = button_clear)
 button_clear.grid(row = 1, column = 0, columnspan = 4, sticky = W)
-button_log = Button (window, text="LOGS", bg="black", fg="white", activebackground="white", activeforeground="black", width="35", command = button_log)
+button_log = Button (window, text = "LOGS", bg = "black", fg = "white", activebackground = "white", activeforeground = "black", width = "35", command = button_log)
 button_log.grid(row = 2, column = 0, columnspan = 4, sticky = W)
-button_load = Button (window, text="LOAD", bg="black", fg="white", activebackground="white", activeforeground="black", width="35", command = load_data)
+button_load = Button (window, text = "LOAD", bg = "black", fg = "white", activebackground = "white", activeforeground = "black", width = "35", command = load_data)
 button_load.grid(row = 3, column = 0, columnspan = 4, sticky = W)
-button_save = Button (window, text="SAVE", bg="black", fg="white", activebackground="white", activeforeground="black", width="35", command = save_data)
+button_save = Button (window, text = "SAVE", bg = "black", fg = "white", activebackground = "white", activeforeground = "black", width = "35", command = save_data)
 button_save.grid(row = 4, column = 0, columnspan = 4, sticky = W)
 
 #Function closing main window.
